f75774ab5baca51a722fdd54f617d169b5173f67
Gabriel Paradiso <gabriel.paradiso@typeform.com>
2018-07-13 14:31:18 +0200

    redux example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return[
+        {title: '1984', pages:90},
+        {title: 'rebelion en la granja', pages:80},
+        {title: 'got', pages:200},
+        {title: 'harry popoter', pages: 180}
+    ]
+}
